dc87e345a460fdf03f3d95d7cea05a18584d844c
Roman Kalyakin <roman@kalyakin.com>
2020-12-23 15:56:41 +0100

    updated code for enc/dec models

--- test/test_generate.py
+++ test/test_generate.py
@@ -1,14 +1,16 @@
 import unittest
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, \
+    T5Tokenizer, T5ForConditionalGeneration
 from tf_step_generator.generator import FirstTokenGenerator
 
-MODEL_NAME = 'distilgpt2'
-
-class GenerateTestCase(unittest.TestCase):
+class GenerateLMTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.tokenizer = GPT2Tokenizer.from_pretrained(MODEL_NAME)
-        cls.model = GPT2LMHeadModel.from_pretrained(MODEL_NAME)
+        MODEL_NAME = 'distilgpt2'
+        ModelClass, TokenizerClass = GPT2LMHeadModel, GPT2Tokenizer
+
+        cls.tokenizer = TokenizerClass.from_pretrained(MODEL_NAME)
+        cls.model = ModelClass.from_pretrained(MODEL_NAME)
 
     def test_generate_one_simple(self):
         generator = FirstTokenGenerator(self.model, self.tokenizer)
@@ -21,7 +23,6 @@ class GenerateTestCase(unittest.TestCase):
         assert next_token_generator is not None
         assert step.sample_token_id() is not None
         assert next_token_generator.get_generated_text() == ''
-        assert next_token_generator.get_prompt_text() == 'In a galaxy'
 
     def test_generate_two_tokens(self):
         generator = FirstTokenGenerator(self.model, self.tokenizer)
@@ -39,7 +40,45 @@ class GenerateTestCase(unittest.TestCase):
         assert step.sample_token_id() is not None
 
         assert next_token_generator.get_generated_text() == '-'
-        assert next_token_generator.get_prompt_text() == 'In a galaxy'
+
+class GenerateConditionalTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        MODEL_NAME = 'valhalla/distilt5-qg-hl-6-4'
+        TokenizerClass, ModelClass = T5Tokenizer, T5ForConditionalGeneration
+
+        cls.tokenizer = TokenizerClass.from_pretrained(MODEL_NAME)
+        cls.model = ModelClass.from_pretrained(MODEL_NAME)
+
+    def test_generate_one_simple(self):
+        generator = FirstTokenGenerator(self.model, self.tokenizer)
+        step, next_token_generator = generator('In a galaxy', top_k=3)
+        assert step.tokens_tuple == [
+            ('In', 0.8047278523445129), 
+            ('What', 0.07219909876585007), 
+            ('Where', 0.12307299673557281)
+        ]
+        assert next_token_generator is not None
+        assert step.sample_token_id() is not None
+        assert next_token_generator.get_generated_text() == ''
+
+    def test_generate_two_tokens(self):
+        generator = FirstTokenGenerator(self.model, self.tokenizer)
+        step, next_token_generator = generator('In a galaxy', top_k=3)
+
+        next_token_id = int(step.next_ids[0].item())
+
+        step, next_token_generator = next_token_generator(next_token_id)
+        assert step.tokens_tuple == [
+            ('', 0.924037754535675),
+            ('the', 0.030165817588567734),
+            ('an', 0.045796316117048264)
+        ]
+        assert next_token_generator is not None
+        assert step.sample_token_id() is not None
+
+        print('|', next_token_generator.get_generated_text(), '|')
+        assert next_token_generator.get_generated_text() == 'In'
 
 
 if __name__ == '__main__':
--- tf_step_generator/generator.py
+++ tf_step_generator/generator.py
@@ -2,7 +2,8 @@ from typing import Optional, Tuple, Any
 from dataclasses import dataclass
 import torch
 import torch.cuda
-from transformers import PreTrainedModel, PreTrainedTokenizer, BatchEncoding
+from transformers import PreTrainedModel, PreTrainedTokenizer
+from transformers.file_utils import ModelOutput
 from tf_step_generator.utils import default_get_next_token_logits, get_logits_processor_list, get_token_ids_and_probabilities
 
 @dataclass
@@ -39,35 +40,40 @@ class TokenGeneratorBase:
 
     def generate_step(
         self,
-        tokenized_input: BatchEncoding,
+        model_kwargs: Any,
         top_k: Optional[int] = 3,
         top_p: Optional[float] = None
-    ) -> Tuple[torch.LongTensor, torch.FloatTensor]:
-        model_kwargs = {
-            **tokenized_input,
-            'use_cache': False
-        }
-        
+    ) -> Tuple[torch.LongTensor, torch.FloatTensor, Any]:
+        input_ids = model_kwargs['input_ids']
+        del model_kwargs['input_ids']
+        model_inputs = self.model.prepare_inputs_for_generation(input_ids, **model_kwargs)
+
         with torch.no_grad():
-            result = self.model(**model_kwargs)
+            result = self.model(**model_inputs, return_dict=True)
 
         logits = default_get_next_token_logits(result)
         logits_processors = get_logits_processor_list(top_k, top_p)
-        processed_logits = logits_processors(model_kwargs['input_ids'], logits)
+        processed_logits = logits_processors(input_ids, logits)
         next_tokens_ids, next_tokens_probabilities = get_token_ids_and_probabilities(processed_logits)
-        
-        return next_tokens_ids, next_tokens_probabilities
+
+        model_kwargs = self.model._update_model_kwargs_for_generation(
+            result, model_kwargs, is_encoder_decoder=self.model.config.is_encoder_decoder
+        )
+
+        model_kwargs['input_ids'] = input_ids
+
+        return next_tokens_ids, next_tokens_probabilities, model_kwargs
 
 class NextTokenGenerator(TokenGeneratorBase):
     def __init__(self,
                  model: PreTrainedModel,
                  tokenizer: PreTrainedTokenizer,
-                 tokenized_input: BatchEncoding,
+                 model_kwargs: Any,
                  prompt_length: int):
         super().__init__(model, tokenizer)
         self.model = model
         self.tokenizer = tokenizer
-        self.tokenized_input = tokenized_input
+        self.model_kwargs = model_kwargs
         self.prompt_length = prompt_length
 
     def __call__(self,
@@ -75,22 +81,17 @@ class NextTokenGenerator(TokenGeneratorBase):
                  top_k: Optional[int] = 3,
                  top_p: Optional[float] = None) -> Tuple[GenerationStepResults, 'NextTokenGenerator']:
 
-        self.tokenized_input['input_ids'] = torch.cat([
-            self.tokenized_input['input_ids'],
+        self.model_kwargs['input_ids'] = torch.cat([
+            self.model_kwargs['input_ids'],
             torch.LongTensor([[next_token_id]]).to(self.model.device),
         ], dim=-1)
 
-        if 'attention_mask' in self.tokenized_input:
-            self.tokenized_input['attention_mask'] = torch.cat([
-                self.tokenized_input['attention_mask'],
-                torch.LongTensor([[1]]).to(self.model.device),
-            ], dim=-1)
-
-        next_tokens_ids, next_tokens_probabilities = self.generate_step(
-            self.tokenized_input,
+        next_tokens_ids, next_tokens_probabilities, model_kwargs = self.generate_step(
+            self.model_kwargs,
             top_k=top_k,
             top_p=top_p
         )
+        self.model_kwargs = model_kwargs
 
         step_results = GenerationStepResults(
             next_tokens_ids,
@@ -101,13 +102,9 @@ class NextTokenGenerator(TokenGeneratorBase):
         return step_results, self
 
     def get_generated_text(self) -> str:
-        return self.tokenizer.decode(self.tokenized_input['input_ids']
-            .squeeze(0)[self.prompt_length:])
-
-    def get_prompt_text(self) -> str:
-        return self.tokenizer.decode(self.tokenized_input['input_ids']
-            .squeeze(0)[:self.prompt_length])
-
+        offset = 0 if self.model.config.is_encoder_decoder else self.prompt_length
+        return self.tokenizer.decode(self.model_kwargs['input_ids']
+            .squeeze(0)[offset:], skip_special_tokens=True)
 
 class FirstTokenGenerator(TokenGeneratorBase):
     def __init__(self,
@@ -123,10 +120,48 @@ class FirstTokenGenerator(TokenGeneratorBase):
         tokenized_input = self.tokenizer(input_text, return_tensors='pt')
         tokenized_input = {k: v.to(self.model.device) for k, v in tokenized_input.items()}
 
+        model_kwargs = {
+            **tokenized_input,
+            'use_cache': False
+        }
+
+        # From: https://github.com/huggingface/transformers/blob/1558d191e66fe3b5b34c8d9a6ce657a39d5133ae/src/transformers/generation_utils.py#L501
+        if self.model.config.is_encoder_decoder:
+            # add encoder_outputs to model_kwargs
+            input_ids = model_kwargs['input_ids']
+            del model_kwargs['input_ids']
+            model_kwargs = self.model._prepare_encoder_decoder_kwargs_for_generation(input_ids, model_kwargs)
+
+            # NOTE: this is originally passed as an argument
+            decoder_start_token_id = None 
+            
+            bos_token_id = self.model.config.bos_token_id
+            # set input_ids as decoder_input_ids
+            model_kwargs['input_ids'] = self.model._prepare_decoder_input_ids_for_generation(
+                input_ids, decoder_start_token_id=decoder_start_token_id, bos_token_id=bos_token_id, **model_kwargs
+            )
+
+            if "encoder_outputs" not in model_kwargs or not isinstance(model_kwargs["encoder_outputs"], ModelOutput):
+                raise ValueError("Make sure that `model_kwargs` include `encoder_outputs` of type `ModelOutput`.")
+
+        # From: https://github.com/huggingface/transformers/blob/1558d191e66fe3b5b34c8d9a6ce657a39d5133ae/src/transformers/generation_utils.py#L564
+        num_return_sequences = 1
+
+        input_ids = model_kwargs['input_ids']
+        del model_kwargs['input_ids']
+
+        input_ids, model_kwargs = self.model._expand_inputs_for_generation(
+            input_ids,
+            expand_size=num_return_sequences,
+            is_encoder_decoder=self.model.config.is_encoder_decoder,
+            **model_kwargs,
+        )
+        model_kwargs['input_ids'] = input_ids
+
         prompt_length = tokenized_input['input_ids'].shape[-1]
 
-        next_tokens_ids, next_tokens_probabilities = self.generate_step(
-            tokenized_input,
+        next_tokens_ids, next_tokens_probabilities, model_kwargs = self.generate_step(
+            model_kwargs,
             top_k=top_k,
             top_p=top_p
         )
@@ -139,7 +174,7 @@ class FirstTokenGenerator(TokenGeneratorBase):
         next_token_generator = NextTokenGenerator(
             self.model,
             self.tokenizer,
-            tokenized_input,
+            model_kwargs,
             prompt_length=prompt_length
         )
 
